117126cf539735926b3e6208a52543a71ccb6622
shahzaib ali <shahzaibalihassan@gmail.com>
2019-12-12 18:55:57 +0500

    Implemented footer

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <script defer src="Js/jquery.min.js"></script>
     <script defer src="Js/app.js"></script>
     <script src="https://use.fontawesome.com/5b17aee48f.js"></script>
+    <link href="css/style.css" rel="stylesheet"/>
 </head>
 <body>
 
@@ -15,7 +16,7 @@
     <div id="navbarBasicExample" class="navbar-menu">
         <div class="navbar-start">
             <a class="navbar-item">
-                DAMAGE  MECHANISM SIMULATOR
+                DAMAGE MECHANISM SIMULATOR
             </a>
         </div>
     </div>
@@ -25,5 +26,18 @@
     <?php include 'Templates/left-tile-section.php'; ?>
     <?php include 'Templates/right-tile-section.php'; ?>
 </div>
+<footer class="main-footer login">
+    <div class="copyrights">
+        <a href="#" data-toggle="modal" data-target="#toc">Terms &amp; Conditions</a> |<a href="#" data-toggle="modal"
+                                                                                          data-target="#aboutus"> About
+            Tool</a> | <a href="mailto:training@rapidsolve.ca">Subscribe</a> | <a href="#" data-toggle="modal"
+                                                                                  data-target="#training"> Training </a>
+        | <span class="copyrights">Rapidsolve™ <span><img src="img/footer-flag.png"></span>Version 3.14 All Rights Reserved © 2016-2019</span>
+        <div class="footer-social">
+            <a href="https://twitter.com/rapidsolve" target="new"><img src="img/twitter.png"></a>
+            <a href="https://www.linkedin.com/company/55181105" target="new"><img src="img/linkedin.png"></a>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
