2a970c82823d084f0a89ba6f1386b1f5b496b0ed
shahzaib ali <shahzaibalihassan@gmail.com>
2019-12-14 21:39:53 +0500

    Implemented new footer

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
     <link href="css/style.css" rel="stylesheet"/>
 </head>
 <body>
-
 <nav class="navbar is-black" role="navigation" aria-label="main navigation">
     <div id="navbarBasicExample" class="navbar-menu">
         <div class="navbar-start">
@@ -21,23 +20,10 @@
         </div>
     </div>
 </nav>
-
 <div class="container">
     <?php include 'Templates/left-tile-section.php'; ?>
     <?php include 'Templates/right-tile-section.php'; ?>
 </div>
-<footer class="main-footer login">
-    <div class="copyrights">
-        <a href="#" data-toggle="modal" data-target="#toc">Terms &amp; Conditions</a> |<a href="#" data-toggle="modal"
-                                                                                          data-target="#aboutus"> About
-            Tool</a> | <a href="mailto:training@rapidsolve.ca">Subscribe</a> | <a href="#" data-toggle="modal"
-                                                                                  data-target="#training"> Training </a>
-        | <span class="copyrights">Rapidsolve™ <span><img src="img/footer-flag.png"></span>Version 3.14 All Rights Reserved © 2016-2019</span>
-        <div class="footer-social">
-            <a href="https://twitter.com/rapidsolve" target="new"><img src="img/twitter.png"></a>
-            <a href="https://www.linkedin.com/company/55181105" target="new"><img src="img/linkedin.png"></a>
-        </div>
-    </div>
-</footer>
+<?php include_once 'footer.html'; ?>
 </body>
 </html>
---
+++ js/custom-scripts.js
@@ -0,0 +1,41 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+$(document).ready(function () {
+    $(".login-form button.custom-stl").click(function (e) {
+        e.preventDefault();
+        var username = $('.login-form .username').val();
+        var password = $('.login-form .password').val();
+        if ((username == 'user' && password == 'user') || (username == 'admin' && password == 'admin')) {
+            window.location.href = 'FFS-landing.html', true;
+        } else {
+            alert('Invalid Login Details. Please contact techsupport@rapidsolve.ca');
+        }
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('header .top-row').addClass("sticky");
+        } else {
+            $('header .top-row').removeClass("sticky");
+        }
+    });
+
+    $('select').select2();
+
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4,.header-menu').mouseover(function () {
+        $("#nav-icon4").addClass('open');
+    });
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4,.header-menu').mouseout(function () {
+        $("#nav-icon4").removeClass('open');
+    });
+});
