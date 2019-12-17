84f692c345e3c0f1c79bdfffee5a91b5ac25d314
shahzaib ali <shahzaibalihassan@gmail.com>
2019-12-17 19:22:28 +0500

    Implemented new header

--- index.php
+++ index.php
@@ -10,27 +10,21 @@
     <link href="css/bootstrap-glyphicons.css" rel="stylesheet"/>
     <link href="css/style.css" rel="stylesheet"/>
 
-    <script src="//code.jquery.com/jquery.min.js"></script>
+    <script src="js/jquery-1.11.1.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/custom-scripts.js"></script>
-    <!--    for multi select dropdowns-->
+    <!--        for multi select dropdowns-->
     <link rel="stylesheet" href="Searchable-Multi-select-jQuery-Dropdown/jquery.dropdown.min.css">
     <script src="Searchable-Multi-select-jQuery-Dropdown/jquery.dropdown.js"></script>
     <script src="https://use.fontawesome.com/5b17aee48f.js"></script>
     <!--    custom scripts-->
     <script defer src="Js/all.js"></script>
     <script defer src="Js/app.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/header.css">
 </head>
 <body>
-<nav class="navbar is-black" role="navigation" aria-label="main navigation">
-    <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-            <a class="navbar-item">
-                DAMAGE MECHANISM SIMULATOR
-            </a>
-        </div>
-    </div>
-</nav>
+<?php include_once 'header.html'; ?>
 <div class="container">
     <?php include 'Templates/left-tile-section.php'; ?>
     <?php include 'Templates/right-tile-section.php'; ?>
